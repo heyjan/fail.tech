9aba7d03d9429b4893d8cadb802a5b829887b7be
heyjan <47437411+heyjan@users.noreply.github.com>
2023-09-17 03:02:17 +0200

    included arkham webhook backend

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, jsonify, request, send_from_directory
+from flask import Flask, request, jsonify, abort, send_from_directory
+import discord
+from discord.ext import tasks
+import asyncio
+from collections import deque
+import threading
 import sqlite3
 
+# Flask and SQLite Setup
 app = Flask(__name__, static_folder='static')
 
 def query_db(query, args=(), one=False):
@@ -40,6 +46,55 @@ def search():
 
     return jsonify(results_dict)
 
+# Discord Setup
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+
+# Initialize the Discord client with the required intents
+client = discord.Client(intents=intents)
+
+BOT_TOKEN = 'MTEzNDI5NjQ2OTY3MTc5MjY5MA.G-qKjO.YEODf5wKa34kVfA-Xwm3aBMOCocMTSsUWAUqfA'
+CHANNEL_ID = 1142072323311144962
+
+# Create a queue to hold messages that we want to send to Discord
+message_queue = deque()
+
+@tasks.loop(seconds=5)
+async def background_task():
+    while message_queue:
+        data = message_queue.popleft()
+        print(f"Sending to Discord: {data}")  # Debug print
+        channel = client.get_channel(CHANNEL_ID)
+        content = f"Transaction Alert: {data}"
+        try:
+            await channel.send(content)
+        except Exception as e:
+            print(f"Error sending message: {e}")
+
+@app.route('/webhook', methods=['POST'])
+def webhook_listener():
+    # ... [Your token validation code here]
+
+    data = request.json
+    print(data)
+
+    # Append data to our queue
+    message_queue.append(data)
+    print(f"Current Queue: {message_queue}")  # Debug print
+
+    return jsonify({"message": "Received and forwarded"}), 200
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user.name}({client.user.id})')
+    background_task.start()
+
+def run_discord():
+    asyncio.run(client.start(BOT_TOKEN))
 
 if __name__ == '__main__':
+    # Start the Discord bot in a separate thread
+    threading.Thread(target=run_discord, daemon=True).start()
+    # Run the Flask app in the main thread
     app.run(host='0.0.0.0')
